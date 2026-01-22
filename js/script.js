ff65751adf4c0d297f0ce764d3fcbdea32772b95
Surbhi Sharma <helios12404@gmail.com>
2026-01-22 10:43:15 +0530

    Add modal functionality for image display

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ document.getElementById('login-btn').addEventListener('click', function () {
     document.getElementsByClassName("login-form-hidden")[0].style.display = "block";
   }
 });
+
+<script>
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImage");
+  const closeBtn = document.querySelector(".close");
+
+  document.querySelectorAll(".card-img img").forEach(img => {
+    img.addEventListener("click", () => {
+      modal.style.display = "flex";
+      modalImg.src = img.src;
+    });
+  });
+
+  closeBtn.onclick = () => modal.style.display = "none";
+
+  modal.onclick = () => modal.style.display = "none";
+</script>
+
